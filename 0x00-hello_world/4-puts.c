ef1c02685cedca14a90ba53ffcc819a7044b45c4
Tigat <demeketigat@gmail.com>
2022-12-08 00:41:40 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  * Return : Always 0 (Success)
  */
 
-int main (void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{ 
+	puts("\"Programming is Like Building A Multilingual Puzzle");
 	return (0);
 }
