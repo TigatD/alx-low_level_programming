f6607c29370c4f7033a49a4f42e01ebd29ad8443
Tigat <demeketigat@gmail.com>
2022-12-12 04:13:34 -0800

    Aa to Zz

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Entry point 
  *
  * Return: Always 0 (success)
  */
@@ -9,11 +9,19 @@ int main(void)
 {
 	char c;
 
+	char d;
+
 	c = 'a';
+	d = 'A';
 	while
 		(c <= 'z') {
 			putchar(c);
 			c++;
+		}
+	while
+		(d <= 'Z') {
+			putchar(c);
+			d++;
 		}
 	putchar('\n');
 	return (0);
