388af8e699532c994daddf69dc35f9a9765d9a8c
Tigat <demeketigat@gmail.com>
2022-12-13 04:36:28 -0800

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
 	_putchar('_');
@@ -17,7 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
   return (0);
 }
 
