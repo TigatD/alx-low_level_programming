708229cfda9bd22920de296466eae2d330debc30
Tigat <demeketigat@gmail.com>
2022-12-13 06:16:45 -0800

    Tens times the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints ten times in lowercase
+ */
+void print_alphabet_x10(void);
+{
+	int ten;
+	char me;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (me = 'a'; me <= 'z'; me++)
+			_putchar(me);
+		_putchar('\n');
+	}
+}
+
